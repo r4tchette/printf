c4177addda7613354ef12d14300d43c0be3393bf
r4tchette <r4tchette@gmail.com>
2021-01-12 20:28:24 +0900

    modify to print pointer with null

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yeonkim <yeonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/19 13:44:30 by yeonkim           #+#    #+#             */
-/*   Updated: 2021/01/12 20:28:04 by yeonkim          ###   ########.fr       */
+/*   Updated: 2021/01/12 20:28:22 by yeonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -300,7 +300,7 @@ int		p_to_str(t_format format, va_list *ap)
 
 	ptr = va_arg(*ap, long long);
 	is_zero = !ptr ? 1 : 0;
-	if (!format.flag['.']);
+	if (!format.flag['.'])
 		format.precision = (!ptr) ? 1 : 12;
 	res = ft_ptoa(ptr, format.precision);
 	if (format.flag['.'] && format.precision <= 0)
