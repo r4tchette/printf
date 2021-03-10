58369fa0aa905b3698f080993f19ad2badae13bf
yeonkim <r4tchette@gmail.com>
2021-03-10 21:18:26 +0900

    align global scope

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yeonkim <yeonkim@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 14:28:38 by yeonkim           #+#    #+#             */
-/*   Updated: 2021/03/10 21:10:53 by yeonkim          ###   ########.fr       */
+/*   Updated: 2021/03/10 21:18:12 by yeonkim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-void			ft_bzero(void *s, size_t n);
-void			*ft_calloc(size_t count, size_t size);
-char			*ft_strdup(const char *s1);
-size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
-size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_t			ft_strlen(const char *s);
-char			*ft_strjoin(char const *s1, char const *s2);
-char			*ft_itoa(int n);
-char			*ft_ltoa(long long n);
-char			*ft_xtoa(unsigned int n, char c);
-char			*ft_utoa(unsigned int n);
-char			*ft_ftoa(double decimal, int precision);
-char            *ft_ptoa(long long ptr, int precision);
-int				ft_strappend(char **s, char const c);
-int				ft_printf(char *str, ...);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlen(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_itoa(int n);
+char	*ft_ltoa(long long n);
+char	*ft_xtoa(unsigned int n, char c);
+char	*ft_utoa(unsigned int n);
+char	*ft_ftoa(double decimal, int precision);
+char	*ft_ptoa(long long ptr, int precision);
+int		ft_strappend(char **s, char const c);
+int		ft_printf(char *str, ...);
 
 #endif
